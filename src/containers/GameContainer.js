748d52a1ca1618bfa36e57b8d0fd71e55047e218
alec-horwitz <horwitz_alec@wheatoncollege.edu>
2018-07-08 11:20:59 -0400

    wrote out the logic for the threeToThree function

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -47,7 +47,47 @@ class GameContainer extends Component {
     }
   }
 
-  threeToThree = (attacker, defender) => {}
+  threeToThree = (attacker, defender) => {
+    let dmg1 = 1
+    let dmg2 = 1
+    let dmg3 = 1
+
+    let attack1 = (Math.floor(Math.random() *
+    Math.floor(10))+1)*
+    (5*typeDmgs[attacker.type1][defender.type1])
+
+    let attack2 = (Math.floor(Math.random() *
+    Math.floor(10))+1)*
+    (5*typeDmgs[attacker.type2][defender.type1])
+
+    let attack3 = (Math.floor(Math.random() *
+    Math.floor(10))+1)*
+    (5*typeDmgs[attacker.type3][defender.type2])
+
+    let defense1 = (Math.floor(Math.random() *
+    Math.floor(10))+1)*
+    (typeDefs[defender.type1][attacker.type1])
+
+    let defense2 = (Math.floor(Math.random() *
+    Math.floor(10))+1)*
+    (typeDefs[defender.type2][attacker.type1])
+
+    let defense3 = (Math.floor(Math.random() *
+    Math.floor(10))+1)*
+    (typeDefs[defender.type3][attacker.type2])
+
+    if (attack1 > defense1) {
+      dmg1 = (attack/3 - defense1)
+    }
+    if (attack2 > defense2) {
+      dmg2 = (attack/3 - defense2)
+    }
+    if (attack3 > defense3) {
+      dmg3 = (attack/3 - defense3)
+    }
+
+    return dmg1+dmg2+dmg3
+  }
   threeToTwo = (attacker, defender) => {
     let dmg1 = 1
     let dmg2 = 1
