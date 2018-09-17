cd81d970f139f9d320154003e57946091cfa8c8d
alec-horwitz <horwitz_alec@wheatoncollege.edu>
2018-09-17 15:43:01 -0400

    removed some unnessisarry comments

--- src/components/NewUserComponent.js
+++ src/components/NewUserComponent.js
@@ -2,36 +2,6 @@ import React, { Component } from 'react';
 import {Form, Container} from 'semantic-ui-react'
 import {connect} from 'react-redux'
 
-// const types = {
-//   Aquarius: 'air',
-//   Gemini: 'air',
-//   Libra: 'air',
-//   Taurus: 'earth',
-//   Virgo: 'earth',
-//   Capricorn: 'earth',
-//   Pisces: 'water',
-//   Cancer: 'water',
-//   Scorpio: 'water',
-//   Aries: 'fire',
-//   Leo: 'fire',
-//   Sagittarius: 'fire'
-// }
-//
-// const Avatars = {
-//   Aquarius: 'https://onehdwallpaper.com/wp-content/uploads/2015/08/Aquarius-Horoscope-Desktop-Backgrounds.jpg',
-//   Gemini: 'https://www.englishclub.com/efl/wp-content/uploads/2011/07/03c-Gemini.png',
-//   Libra: 'https://1.bp.blogspot.com/-dVGP128g-ns/UbTyCKx2H9I/AAAAAAAAAl0/9DelDJUENCU/s1600/Libra.jpg',
-//   Taurus: 'https://2.bp.blogspot.com/_01YItv3gFfo/S8R4S2NwnMI/AAAAAAAAAJY/eqScVtI_Zb4/s1600/Taurus-Zodiac-Sign-1.jpeg',
-//   Virgo: 'https://1.bp.blogspot.com/-C3Wv_yKA4U8/TiRe7toN5lI/AAAAAAAACT4/HEm3QnRw1k4/s1600/virgo11.jpg',
-//   Capricorn: 'https://1.bp.blogspot.com/-vRML1Dg9ECg/UWg6fAv0EcI/AAAAAAAAFFo/3WaNG6V8j9s/s1600/zodiac1+capricorn.jpg',
-//   Pisces: 'https://4.bp.blogspot.com/-XAXArbVtBcY/UD9tPBPIfiI/AAAAAAAAIvY/ZVGw9jSoCoo/s1600/Pisces+Wallpapers.jpg',
-//   Cancer: 'https://4.bp.blogspot.com/-jF9rRjCTB-w/UI7svYw69uI/AAAAAAAATyM/X-2h4w_kQSg/s1600/4Cancer-Zodiac-Sign-1.jpeg',
-//   Scorpio: 'http://pixfeeds.com/images/18/463850/1200-614609158-scorpio-sign.jpg',
-//   Aries: 'https://2.bp.blogspot.com/_WeQf3HIdYws/S_YL1j9bncI/AAAAAAAAAAc/1M-mJkzfMng/s1600/aries.jpg',
-//   Leo: 'https://2.bp.blogspot.com/-1_2dylpfIqg/UV_hyTbRl_I/AAAAAAAAFAI/iZChMtwrNvo/s1600/Leo-Zodiac-Sign-1.jpeg',
-//   Sagittarius: 'https://2.bp.blogspot.com/-obkvSdEM3jY/UWaJwGUfL7I/AAAAAAAAFEc/fTgiP_0KJaE/s1600/Sagittarius.jpeg'
-// }
-
 const options = [
   { key: 'aqu', text: 'Aquarius', value: 'Aquarius' },
   { key: 'gem', text: 'Gemini', value: 'Gemini' },
